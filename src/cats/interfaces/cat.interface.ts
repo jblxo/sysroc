8a8f4e9501142f4bce0174a6f8ad3f06d47522e3
jblxo <hliba.ondra@live.com>
2019-10-23 08:57:42 +0200

    Remove Cats Demo

--- src/cats/interfaces/cat.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface Cat extends Document {
-  readonly id: string;
-  readonly name: string;
-  readonly age: number;
-  readonly breed: string;
-}
