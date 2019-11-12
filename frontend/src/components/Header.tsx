3870fed87bce338b32775c5bfa47c3f19c26362d
jblxo <hliba.ondra@live.com>
2019-11-12 08:44:56 +0100

    Add basic navbar with logout button on user icon menu

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,53 @@
 import React from 'react';
 import { useMeQuery, useLogoutMutation } from '../generated/graphql';
 import { Link } from 'react-router-dom';
-import { Button } from '@material-ui/core';
+import {
+  Button,
+  Toolbar,
+  AppBar,
+  Menu,
+  MenuItem,
+  FormGroup,
+  FormControlLabel,
+  IconButton,
+  Typography,
+  Switch
+} from '@material-ui/core';
 import { setAccessToken } from '../auth/accessToke';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    title: {
+      flexGrow: 1
+    }
+  })
+);
 
 interface Props {}
 
 export const Header: React.FC<Props> = () => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
   const { data, loading } = useMeQuery();
   const [logout, { client }] = useLogoutMutation();
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   let body: any = null;
 
@@ -21,27 +60,62 @@ export const Header: React.FC<Props> = () => {
   }
 
   return (
-    <header>
-      <div>
-        <Link to="/home"></Link>
-      </div>
-      <div>
-        <Link to="/singin"></Link>
-      </div>
-      <div>
-        {!loading && data && data.me ? (
-          <Button
-            onClick={async () => {
-              await logout();
-              setAccessToken('');
-              await client!.resetStore();
-            }}
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
           >
-            Logout
-          </Button>
-        ) : null}
-      </div>
-      {body}
-    </header>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Sysroc
+          </Typography>
+          {!loading && data && data.me && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem
+                  onClick={async () => {
+                    await logout();
+                    setAccessToken('');
+                    await client!.resetStore();
+                  }}
+                >
+                  Logout
+                </MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
