ad0d6c1890d0e23b853ec7c0e410a89ec4729746
jblxo <hliba.ondra@live.com>
2019-12-18 15:16:37 +0100

    Add Left Side Drawer Template

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -12,24 +12,44 @@ import { setAccessToken } from '../auth/accessToke';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import clsx from 'clsx';
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      flexGrow: 1
-    },
     menuButton: {
       marginRight: theme.spacing(2)
     },
     title: {
       flexGrow: 1
+    },
+    appBar: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      })
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen
+      })
+    },
+    hide: {
+      display: 'none'
     }
   })
 );
 
-interface Props {}
+interface Props {
+  handleDrawerOpen: () => void;
+  drawerOpen: boolean;
+}
 
-export const Header: React.FC<Props> = () => {
+export const Header: React.FC<Props> = props => {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
@@ -45,63 +65,67 @@ export const Header: React.FC<Props> = () => {
   };
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Sysroc
-          </Typography>
-          {!loading && data && data.me && (
-            <div>
-              <IconButton
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right'
+    <AppBar
+      position="fixed"
+      className={clsx(classes.appBar, {
+        [classes.appBarShift]: props.drawerOpen
+      })}
+    >
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={clsx(classes.menuButton, props.drawerOpen && classes.hide)}
+          color="inherit"
+          aria-label="menu"
+          onClick={props.handleDrawerOpen}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          Sysroc
+        </Typography>
+        {!loading && data && data.me && (
+          <div>
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+              <MenuItem
+                onClick={async () => {
+                  await logout();
+                  setAccessToken('');
+                  await client!.resetStore();
+                  handleClose();
                 }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right'
-                }}
-                open={open}
-                onClose={handleClose}
               >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-                <MenuItem
-                  onClick={async () => {
-                    await logout();
-                    setAccessToken('');
-                    await client!.resetStore();
-                    handleClose();
-                  }}
-                >
-                  Logout
-                </MenuItem>
-              </Menu>
-            </div>
-          )}
-        </Toolbar>
-      </AppBar>
-    </div>
+                Logout
+              </MenuItem>
+            </Menu>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { SignIn } from '../components/SignIn';
 import { Home } from '../components/Home';
-import { Header } from '../components/Header';
 import { SignUp } from '../components/SignUp';
+import { PersistentDrawerLeft } from '../components/PersisstentDrawerLeft';
 
 export const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/signin" component={SignIn} />
-          <Route exact path="/signup" component={SignUp} />
-        </Switch>
+        <PersistentDrawerLeft>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/signup" component={SignUp} />
+          </Switch>
+        </PersistentDrawerLeft>
       </div>
     </BrowserRouter>
   );
